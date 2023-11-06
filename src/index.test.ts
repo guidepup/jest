9bca0e7ec510c3df194b34ab7a93ecdfd3afdeef
Craig Morten <craig.morten@hotmail.co.uk>
2023-11-06 11:40:57 +0000

    feat: implement screen reader output matchers

---
+++ src/index.test.ts
@@ -0,0 +1,124 @@
+import "./";
+
+function setupBasicPage() {
+  document.body.innerHTML = `
+  <nav>Nav Text</nav>
+  <section>
+    <h1>First Section Heading</h1>
+    <p>First Section Text</p>
+    <article>
+      <header>
+        <h1>Article Header Heading</h1>
+        <p>Article Header Text</p>
+      </header>
+      <p>Article Text</p>
+    </article> 
+  </section>
+  <section>
+    <h1>Second Section Heading</h1>
+    <p>Second Section Text</p>
+  </section>
+  <section aria-hidden="true">
+    <h1>Hidden Section Heading</h1>
+    <p>Hidden Section Text</p>
+  </section>
+  <footer>Footer</footer>
+  `;
+}
+
+describe("matchers", () => {
+  beforeEach(() => {
+    setupBasicPage();
+  });
+
+  afterEach(() => {
+    document.body.innerHTML = ``;
+  });
+
+  test("toMatchScreenReaderSnapshot on the whole body", async () => {
+    await expect(document.body).toMatchScreenReaderSnapshot();
+  });
+
+  test("toMatchScreenReaderInlineSnapshot on the whole body", async () => {
+    await expect(document.body).toMatchScreenReaderInlineSnapshot(`
+[
+  "document",
+  "navigation",
+  "Nav Text",
+  "end of navigation",
+  "region",
+  "heading, First Section Heading, level 1",
+  "First Section Text",
+  "article",
+  "banner",
+  "heading, Article Header Heading, level 1",
+  "Article Header Text",
+  "end of banner",
+  "Article Text",
+  "end of article",
+  "end of region",
+  "region",
+  "heading, Second Section Heading, level 1",
+  "Second Section Text",
+  "end of region",
+  "contentinfo",
+  "Footer",
+  "end of contentinfo",
+  "end of document",
+]
+`);
+  });
+
+  test("toMatchScreenReaderSnapshot on an element", async () => {
+    await expect(
+      document.getElementsByTagName("section")[1]
+    ).toMatchScreenReaderSnapshot();
+  });
+
+  test("toMatchScreenReaderInlineSnapshot on an element", async () => {
+    await expect(document.getElementsByTagName("section")[1])
+      .toMatchScreenReaderInlineSnapshot(`
+[
+  "region",
+  "heading, Second Section Heading, level 1",
+  "Second Section Text",
+  "end of region",
+]
+`);
+  });
+
+  test("toMatchScreenReaderSnapshot on a text node", async () => {
+    await expect(
+      document.getElementsByTagName("nav")[0].firstChild
+    ).toMatchScreenReaderSnapshot();
+  });
+
+  test("toMatchScreenReaderInlineSnapshot on a text node", async () => {
+    await expect(document.getElementsByTagName("nav")[0].firstChild)
+      .toMatchScreenReaderInlineSnapshot(`
+[
+  "Nav Text",
+]
+`);
+  });
+
+  test("toMatchScreenReaderSnapshot on a hidden node", async () => {
+    await expect(
+      document.querySelector('[aria-hidden="true"]')
+    ).toMatchScreenReaderSnapshot();
+  });
+
+  test("toMatchScreenReaderInlineSnapshot on a hidden node", async () => {
+    await expect(
+      document.querySelector('[aria-hidden="true"]')
+    ).toMatchScreenReaderInlineSnapshot(`[]`);
+  });
+
+  test("toMatchScreenReaderSnapshot on a null node", async () => {
+    await expect(null).toMatchScreenReaderSnapshot();
+  });
+
+  test("toMatchScreenReaderInlineSnapshot on a null node", async () => {
+    await expect(null).toMatchScreenReaderInlineSnapshot(`[]`);
+  });
+});
--- src/test/jest.setup.ts
+++
@@ -1,4 +0,0 @@
-import "@testing-library/jest-dom";
-import "@testing-library/jest-dom/jest-globals";
-
-jest.setTimeout(10000);
